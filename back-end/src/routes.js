8985309b6e165659585c588c4505e9e5b12fb777
vitor <vitorpaulasantos@gmail.com>
2019-10-02 14:12:52 -0300

    aplicando as reservas de cada usuario no spot escolhido

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const ProfileController = require('./controllers/ProfileController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -15,5 +16,7 @@ routes.post('/spots',upload.single('thumbnail'), SpotController.store);
 routes.get('/spots', SpotController.index);
 
 routes.get('/profile', ProfileController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
