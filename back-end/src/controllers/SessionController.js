bc34c9595769e297e09cc0cc7f56fd2cf58f3c65
vitor <vitorpaulasantos@gmail.com>
2019-10-02 11:56:16 -0300

    criando o registro de usuarios

---
+++ back-end/src/controllers/SessionController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+
+module.exports = {
+   //index
+
+   //show
+
+   //store
+   async store(req, res){
+      const { email } = req.body;
+
+      let user = await User.findOne({ email });
+
+      if (!user){
+         user = await User.create({ email });
+      }
+
+      return res.json(user);
+   }
+   //update
+
+   //destroy
+};
